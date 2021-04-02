21aa0c35dc51cf15ac5507e1d0fe79f571da8e98
Sateesh Peetha <sateesh.peetha@gmail.com>
2021-04-02 19:43:42 +0530

    added helmet for security headers

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require("helmet");
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +19,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use((req, res) => {
+    res.status(404).send('404: Page not found');
+});
 
 module.exports = app;
